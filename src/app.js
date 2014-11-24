2b61115411a13665e2ae62ebbb26c4939ffdfe9e
Armno <monkeyarmno@gmail.com>
2014-11-24 18:53:01 +0700

    add second test: greeting people properly based on their country.

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@
 	angular.module('myApp', [])
 		.factory('Person', Person);
 
-	function Person() {
+	function Person(visitor) {
 		return function Person(name) {
 			this.name = name;
+			this.greet = function() {
+				if (visitor.country === 'UK') {
+					return 'Good day to you, ' + this.name + '.';
+				} else {
+					return 'Hey, ' + this.name + '!';
+				}
+			};
 		};
 	}
 })();
--- test/unit/personSpec.js
+++ test/unit/personSpec.js
@@ -1,17 +1,36 @@
 describe('Person', function() {
 
-	var Person;
+	// local reference to injected services
+	var Person, visitor;
 
+	// bring in our app's module
 	beforeEach(module('myApp'));
+
+	beforeEach(module(function($provide) {
+		visitor = {};
+		$provide.value('visitor', visitor);
+	}));
 
 	beforeEach(inject(function(_Person_) {
 		Person = _Person_;
 	}));
 
 	describe('Constructor', function() {
-
 		it('assigns a name', function() {
 			expect(new Person('Ben')).to.have.property('name', 'Ben');
 		});
+	});
+
+	describe('#greet', function() {
+		it('greets UK people formally', function() {
+			visitor.country = 'UK';
+			expect(new Person('Nigel').greet()).to.equal('Good day to you, Nigel.');
+		});
+
+		it('greets US people hipsterly', function() {
+			visitor.country = 'US';
+			expect(new Person('Ben').greet()).to.equal('Hey, Ben!');
+		});
+
 	});
 });
